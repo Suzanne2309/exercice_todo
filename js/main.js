c4b43f7dda0d65a5ce233b37da1857c9ed9ec0da
Suzanne <suzanne.pons68@gmail.com>
2025-09-17 15:58:42 +0200

    ajout fonction création nouvelle task et suppression

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const addBtn = document.querySelector('#btn');
 //On crée l'événement du click sur le bouton d'ajout qui provoquera la fonction addTask
 addBtn.addEventListener('click', addTask);
 
+
 //On crée la fonction d'ajout (addTask)
 
 //On commence par crée les variables indiquant de chercher l'id ou l'attribut dans le DOM
@@ -14,11 +15,34 @@ const tasksContainer = document.querySelector('#todoCards');
 function addTask() {
     //création de la variable newTask qui va créer une copie de taskCard mais avec cloneNode cela compte comme un élément à lui-même
     const newTask = taskCard.cloneNode(true)
+    //On créer la variable newDelBtn qui va accéder au DOM pour ajouter le bouton de suppression aux nouvelles tasks
+    const newDelBtn = newTask.querySelector('.delBtn')
     //création de la variable newTextAria qui va utiliser la variable newTask pour retrouver l'attribut task du clone "taskCard" dans le DOM 
     const newTextArea = newTask.querySelector('.task')
 
     //Ici la newTextAra va prendre la valeur de nouvelle task et donc ainsi devenir la nouvelle task
     newTextArea.value = "New Task"
-    //Cette action va relier la nouvelle task au tasks Container, en tant que "enfant"
+    //On créer l'événement de click contentant la fonction de suppression de la task pour que cela s'applique sur les nouvelles tasks
+    newDelBtn.addEventListener('click', function () {
+        deleteTask(newTask);
+    })
+
+    //Cette action va ajouter la nouvelle carte dans le DOM, en la reliant au tasks Container en tant que "enfant".
     tasksContainer.appendChild(newTask)
 }
+
+
+//Suppression des cartes to do
+
+//Pour faire fonction le bouton de suppression (icône poubelle)
+const delBtn = document.querySelector('delBtn');
+delBtn.addEventListener('click', function () {
+    deleteTask(taskCard);
+});
+
+//On crée la fonction delteTask
+
+function deleteTask(task) {
+    task.remove();
+}
+
